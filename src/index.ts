64f8e690e469979d4bf2ab2e4f7183428283b6d1
gongzixi <1079688386@qq.com>
2021-12-29 13:42:37 +0800

    path

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@
  * @Author: WangAnCheng 1079688386@qq.com
  * @Date: 2021-12-14 15:53:07
  * @Last Modified by: WangAnCheng 1079688386@qq.com
- * @Last Modified time: 2021-12-29 13:41:30
+ * @Last Modified time: 2021-12-29 13:42:21
  */
-import WaterFall from "./src/waterfall";
+import WaterFall from "./waterfall";
 export default {
   WaterFall,
 };
