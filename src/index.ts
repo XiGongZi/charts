8095f82567c179c071fe7a55b72c92c60f93c123
gongzixi <1079688386@qq.com>
2021-12-29 13:41:56 +0800

    add empty files

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@
  * @Author: WangAnCheng 1079688386@qq.com
  * @Date: 2021-12-14 15:53:07
  * @Last Modified by: WangAnCheng 1079688386@qq.com
- * @Last Modified time: 2021-12-14 16:20:07
+ * @Last Modified time: 2021-12-29 13:41:30
  */
-import WaterFall from "./src/waterfall.js";
+import WaterFall from "./src/waterfall";
 export default {
   WaterFall,
 };
