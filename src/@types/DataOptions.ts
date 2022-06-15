0be0c9927c132a00a8e661fa75087b57d0812750
gongzixi <1079688386@qq.com>
2022-06-15 23:44:11 +0800

    DataOptions interface

--- src/@types/DataOptions.ts
+++ src/@types/DataOptions.ts
@@ -1,11 +1,11 @@
-abstract class DataOptions {
-    calcOptions: CalcOptions = CalcOptions.constructor();
+interface DataOptions {
+    calcOptions: CalcOptions;
     // 元数据
-    originData: IInputDataArr[] = [];
+    originData: IInputDataArr[];
     // 设置属性
-    setOptions(): void { }
+    setOptions: () => void;
     // 重置属性
-    reset(): void { }
+    reset: () => void;
     // 计算相应位置
-    getPosition(): void { }
+    getPosition: () => void;
 }
